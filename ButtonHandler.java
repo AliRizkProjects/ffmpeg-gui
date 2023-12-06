2a62272007426cb2c572a8c6ac4471f748bb53ac
Kentaki <arizk@outlook.de>
2023-12-07 00:00:27 +0100

    ButtonHandler update

--- ButtonHandler.java
+++ ButtonHandler.java
@@ -1,7 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.awt.*;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
