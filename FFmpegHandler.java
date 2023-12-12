62a2d45a5b6fc4841d32f1005458a91774674a3a
Long Huynh Huu <long@putputlaw.de>
2023-12-12 01:08:10 +0100

    minimise how much of FFmpegGUI internals get exposed

--- FFmpegHandler.java
+++ FFmpegHandler.java
@@ -3,36 +3,44 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingWorker;
 
 public class FFmpegHandler extends SwingWorker<Void, String> {
-    FFmpegGUI gui;
+    private String videoFilePath;
+    private String videoOutputPath;
+    private String videoOutputFilename;
+    private String crfOption;
 
-    public FFmpegHandler(FFmpegGUI gui) {
-        this.gui = gui;
+    private JFrame mainFrame;
+    private JTextArea cmdArea;
+
+    public FFmpegHandler(JFrame mainFrame, JTextArea cmdArea, String videoFilePath, String videoOutputPath,
+            String videoOutputFilename, String crfOption) {
+        this.mainFrame = mainFrame;
+        this.cmdArea = cmdArea;
+        this.videoFilePath = videoFilePath;
+        this.videoOutputPath = videoOutputPath;
+        this.videoOutputFilename = videoOutputFilename;
+        this.crfOption = crfOption;
     }
 
     protected Void doInBackground() throws Exception {
-        Process process = null;
-        
-        String videoFilePath = gui.getVideoPathTextField().getText();
-        String videoOutputPath = gui.getOutputPathTextField().getText(); 
-        String crfOption = (String) gui.getCrfComboBox().getSelectedItem();
-
         // check if selected outputname already exists in selected outputfolder
-        String checkFilename = gui.getOutputName().getText();
-        Path checkFilepath = Paths.get(videoOutputPath, checkFilename);
-        if (Files.exists(checkFilepath)) {
-            JOptionPane.showMessageDialog(gui.getMainframe(),
+        Path videoOutputFilepath = Paths.get(videoOutputPath, videoOutputFilename);
+        if (Files.exists(videoOutputFilepath)) {
+            JOptionPane.showMessageDialog(mainFrame,
                     "Sorry, but the chosen filename already exists. Please pick a different name.",
                     "File already exists", JOptionPane.WARNING_MESSAGE);
         } else {
             // create and run process
             ProcessBuilder pb = new ProcessBuilder("ffmpeg", "-i", videoFilePath, "-vcodec", "libx264", "-crf",
-                    crfOption, checkFilepath.toString());
+                    crfOption, videoOutputFilepath.toString());
             pb.redirectErrorStream(true);
+
+            Process process = null;
             try {
                 process = pb.start();
 
@@ -40,12 +48,14 @@ public class FFmpegHandler extends SwingWorker<Void, String> {
                 String line;
                 while (!isCancelled() && (line = reader.readLine()) != null) {
                     System.out.println(line);
-                    gui.getCmdArea().append(line);
-                    gui.getCmdArea().setCaretPosition(gui.getCmdArea().getDocument().getLength());
+                    cmdArea.append(line);
+                    cmdArea.setCaretPosition(cmdArea.getDocument().getLength());
                 }
             } finally {
                 if (isCancelled()) {
-                    process.destroy();
+                    if (process != null) {
+                        process.destroy();
+                    }
                     gui.getCmdArea().append("\n!!!!! PROCESS STOPPED !!!!!");
                     gui.getCmdArea().setCaretPosition(gui.getCmdArea().getDocument().getLength());
                 }
