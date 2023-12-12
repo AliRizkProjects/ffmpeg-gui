6511536b64cca3efc151cf7c8906500f736b5705
Kentaki <arizk@outlook.de>
2023-12-12 13:11:53 +0100

    removed unused libraries

--- VideoStats.java
+++ VideoStats.java
@@ -1,7 +1,5 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.nio.file.Files;
 import java.io.IOException;
 import java.io.BufferedReader;
